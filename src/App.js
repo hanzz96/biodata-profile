bdf6bc85795cb3c505a643dff4d493f33a9123f1
hanzz96 <johanandreas74@yahoo.com>
2021-01-07 13:00:46 +0700

    hanzz96
    init commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+class App extends React.Component {
+  render(){
+	  return (
+		<div className="App">
+		  <header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<p>
+			  Edit <code>src/App.js</code> and save to reload.
+			</p>
+			<a
+			  className="App-link"
+			  href="https://threejs.org/"
+			  target="_blank"
+			  rel="noopener noreferrer"
+			>
+			  Hello ThreeJS!
+			</a>
+		  </header>
+		</div>
+	  );s
+  }
+  
+}
+
+export default App;
