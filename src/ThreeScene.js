494a2bb4f89024009af76774950dad2dcd48d01d
hanzz96 <johanandreas74@yahoo.com>
2021-01-19 17:32:14 +0700

    add load model

--- src/ThreeScene.js
+++ src/ThreeScene.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { GUI } from 'three/examples/jsm/libs/dat.gui.module';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+const temp = new THREE.Vector3();
+let playerX = 3449;
+let playerY = 0;
+let playerZ = 1328;
+let modelReady = false;
 class ThreeScene extends Component {
+  
   constructor(props) {
     super(props)
     this.state = {
@@ -20,11 +27,13 @@ class ThreeScene extends Component {
   };
 
   start = () => {
+    //got called animate method
     if (!this.frameId) {
       this.frameId = requestAnimationFrame(this.animate);
     }
   };
-  stop  ()  {
+
+  stop() {
     cancelAnimationFrame(this.frameId);
   };
 
@@ -32,32 +41,42 @@ class ThreeScene extends Component {
     //Animate Models Here
     //ReDraw Scene with Camera and Scene Object
     this.renderScene();
+    this.stats.update();
+    if(this.model) {
+      temp.setFromMatrixPosition(this.model.matrixWorld);
+      this.camera.lookAt(this.model.position);
+      this.camera.position.lerp(temp, 0.03);
+      this.camera.position.y = playerY + 1000;
+      this.camera.position.z = playerZ + 2500;
+    }
+    if(modelReady){
+
+    }
     this.frameId = window.requestAnimationFrame(this.animate);
   };
 
   createGUI(controller) {
-    this.gui = new GUI({name: 'My GUI'});
+    this.gui = new GUI({ name: 'My GUI' });
 
     if (!this.gui) {
       return;
     }
     const cameraFolder = this.gui.addFolder('Camera');
-    cameraFolder.add( controller, 'x').onChange((inputX) => this.camera.position.x = inputX).listen();
-    cameraFolder.add( controller , 'y').onChange((inputY) => this.camera.position.y = inputY).listen();
-    cameraFolder.add(controller , 'z').onChange((inputZ) => this.camera.position.z = inputZ).listen();
-    console.log(this.gui)
+    cameraFolder.add(controller, 'x').onChange((inputX) => this.camera.position.x = inputX).listen();
+    cameraFolder.add(controller, 'y').onChange((inputY) => this.camera.position.y = inputY).listen();
+    cameraFolder.add(controller, 'z').onChange((inputZ) => this.camera.position.z = inputZ).listen();
   }
 
   init() {
     // SCENE
-
+    
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color( 0xcce0ff );
-    this.scene.fog = new THREE.Fog( 0xcce0ff, 500, 10000 );
+    this.scene.background = new THREE.Color(0xcce0ff);
+    this.scene.fog = new THREE.Fog(0xcce0ff, 500, 10000);
 
     //camera
     this.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 10000);
-    this.camera.position.set(1542, 1202, 2057);
+    this.camera.position.set(playerX, 2323, playerZ);
     this.scene.add(this.camera);
 
     //material
@@ -65,22 +84,80 @@ class ThreeScene extends Component {
     const groundTexture = loader.load('assets/aerial_beach_01_ao_1k.jpg', (asd) => {
       console.log("onload", asd);
     });
-    console.log(loader);
+
     groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
-    groundTexture.repeat.set(25, 25);
+    groundTexture.repeat.set(10, 10);
     groundTexture.anisotropy = 16;
     groundTexture.encoding = THREE.sRGBEncoding;
 
     const groundMaterial = new THREE.MeshBasicMaterial({ map: groundTexture });
     console.log(groundMaterial)
     //ground
-    
+
     let mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(20000, 20000), groundMaterial);
-    mesh.position.y = - 250;
+    mesh.position.y = - -1;
     mesh.rotation.x = - Math.PI / 2;
     // mesh.receiveShadow = true;
     this.scene.add(mesh);
 
+    let light = new THREE.HemisphereLight(0xffffff, 0x444444);
+
+    light.position.set(3449, 2323, 1328);
+
+    this.scene.add(light);
+
+    light = new THREE.DirectionalLight(0xffffff);
+
+    light.position.set(3449, 2323, 1328);
+
+    light.castShadow = true;
+
+    light.shadow.camera.top = 180;
+
+    light.shadow.camera.bottom = - 100;
+
+    light.shadow.camera.left = - 120;
+
+    light.shadow.camera.right = 120;
+
+    this.scene.add(light);
+    //load model
+    let fbxLoader = new FBXLoader();
+    fbxLoader.load('object/remy.fbx', (object) => {
+      //TODO: animation mixer
+      // this.scene.add(object);
+      fbxLoader.load('object/remy@running.fbx', (object) => {
+
+        fbxLoader.load('object/remy@backward.fbx', (object) => {
+
+          object.scale.set(1, 1, 1)
+          object.position.x = 3449;
+          object.position.y = 0;
+          object.position.z = 1328;
+          this.model = object;
+          this.scene.add(object);
+          modelReady = true;
+        })
+      })
+    })
+
+    //glb format
+    // this.gltfLoader = new GLTFLoader();
+    // this.gltfLoader.load('object/RobotExpressive.glb', (object) => {
+    //   model = object.scene;
+    //   model.scale.set(30, 30, 30);
+    //   model.position.x = 303;
+    //   model.position.y = 0;
+    //   model.position.z = 860;
+    //   console.log(model, "test");
+    // this.camera.lookAt(model);
+    //   this.scene.add(model);
+
+    // }, undefined, function (e) {
+
+    //   console.error(e);
+
+    // });
 
     let container = document.createElement('div');
     this.mount.appendChild(container);
@@ -97,16 +174,16 @@ class ThreeScene extends Component {
     this.cameraControls = new OrbitControls(this.camera, this.renderer.domElement);
     //camera controls
     let controllerGUI = {
-      x : this.cameraControls.object.position.x,
+      x: this.cameraControls.object.position.x,
       y: this.cameraControls.object.position.y,
-      z : this.cameraControls.object.position.z
+      z: this.cameraControls.object.position.z
     }
     this.cameraControls.enableKeys = true;
     this.cameraControls.maxPolarAngle = Math.PI * 0.5;
     this.cameraControls.minDistance = 1000;
     this.cameraControls.maxDistance = 5000;
     //this listener for dev only
-    // this.cameraControls.addEventListener('change', (test) => this.orbitControlsChange(test, this.gui.getRoot().__folders.Camera.__listening))
+    this.cameraControls.addEventListener('change', (test) => this.orbitControlsChange(test, this.gui.getRoot().__folders.Camera.__listening))
     this.cameraControls.update()
     this.createGUI(controllerGUI);
 
@@ -114,9 +191,9 @@ class ThreeScene extends Component {
     this.renderScene();
   }
 
-  orbitControlsChange(orbit, gui){
-    console.log(orbit, gui);
-    for(const [key, index] of gui.entries()){
+  orbitControlsChange(orbit, gui) {
+
+    for (const [key, index] of gui.entries()) {
       index.object = orbit.target.object.position;
     }
   }
@@ -125,6 +202,7 @@ class ThreeScene extends Component {
     this.init();
     this.start();
   }
+
   render() {
 
     return (
