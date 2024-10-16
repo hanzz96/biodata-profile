5381ae46f4d992b330beced0d5523297f45e97e9
Johan <johanandreas74@gmail.com>
2024-10-16 18:47:07 +0700

    Object loaded for main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,53 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import { Scroll, ScrollControls } from "@react-three/drei";
+import { Scroll, ScrollControls, OrbitControls } from "@react-three/drei";
 import { Experience } from "./components/Experience";
 import { Interface } from "./components/Interface";
 import { ScrollManager } from "./components/ScrollManager";
 import { Menu } from "./components/Menu";
+import { MotionConfig } from "framer-motion";
 
 function App() {
   const [section, setSection] = useState(0);
   const [menuOpened, setMenuOpened] = useState(false);
-  
+
   useEffect(() => {
     setMenuOpened(false);
   }, [section]);
 
   return (
     <>
-      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-        <color attach="background" args={["#ececec"]} />
-        <ScrollControls pages={5} damping={0.3}>
-          <ScrollManager
-            section={section}
-            onSectionChange={setSection}
-          ></ScrollManager>
-          <Experience />
-          <Scroll html>
-            <Interface></Interface>
-          </Scroll>
-        </ScrollControls>
-      </Canvas>
-      <Menu
-        onSectionChange={setSection}
-        menuOpened={menuOpened}
-        setMenuOpened={setMenuOpened}
-      ></Menu>
+      <MotionConfig
+        transition={{
+          type: "string",
+          mass: 5,
+          stiffness: 500,
+          damping: 50,
+          restDelta: 0.0001,
+        }}
+      >
+        {/* FFF9B5 */}
+        <Canvas shadows shadowMap camera={{ position: [3, 15, 20], fov: 30 }}>
+          <ScrollControls pages={5} damping={0.3}>
+            <ScrollManager
+              section={section}
+              onSectionChange={setSection}
+            ></ScrollManager>
+            <Experience section={section} />
+
+            {/* <OrbitControls /> */}
+            <Scroll html>
+              <Interface></Interface>
+            </Scroll>
+          </ScrollControls>
+        </Canvas>
+        <Menu
+          onSectionChange={setSection}
+          menuOpened={menuOpened}
+          setMenuOpened={setMenuOpened}
+        ></Menu>
+      </MotionConfig>
     </>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,34 @@
 import { OrbitControls } from "@react-three/drei";
+import { GamingRoomBeta } from "./GamingRoomBeta";
+import { motion } from "framer-motion-3d";
 
-export const Experience = () => {
+const positionLight = [7, 7, -1];
+
+export const Experience = (props) => {
+  const { section } = props;
   return (
     <>
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <motion.group
+        position={[5.5, 15, 3]}
+        scale={[0.9, 0.9, 0.9]}
+        rotation-y={-Math.PI / 4.5}
+        animate={{
+          y: section === 0 ? 0 : -1,
+        }}
+      >
+        <directionalLight
+          position={[3, 7, -1]}
+          castShadow
+          shadow-mapSize-width={1024} // Adjust shadow resolution
+          shadow-mapSize-height={1024} // Adjust shadow resolution
+          shadow-camera-near={0.5} // Adjust shadow camera properties
+          shadow-camera-far={20}
+          shadow-bias={-0.0001}
+        />
+        <pointLight position={[3, 7, -1]} power={500} />
+        <ambientLight intensity={0.5} />
+        <GamingRoomBeta></GamingRoomBeta>
+      </motion.group>
     </>
   );
 };
