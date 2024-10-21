729e4b4875d6d0fdcb6d06e154177919e046bfce
Johan <johanandreas74@gmail.com>
2024-10-21 15:36:16 +0700

    setting up environment and add gitignore

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,12 @@
-
 import { GamingRoomBeta } from "./GamingRoomBeta";
 import { motion } from "framer-motion-3d";
 import { useEffect, useRef, useState } from "react";
 import { useControls } from "leva";
 
 function Light() {
+  console.log(import.meta.env.VITE_CONTROLS_ENV)
+  const isDevelopment = import.meta.env.VITE_CONTROLS_ENV === "development";
+
   const ref = useRef();
   const [directionalPosition, setDirectionalPosition] = useState([
     13.1, 12.0, 10.5,
@@ -18,44 +20,53 @@ function Light() {
   ]);
 
   const [pointCameraSetting, setPointCamera] = useState([3, 7, -1, 500]);
-  const { x, y, z, intensity } = useControls("directionalLight settings", {
-    x: { value: directionalPosition[0], min: -50, max: 50, step: 0.1 },
-    y: { value: directionalPosition[1], min: -50, max: 50, step: 0.1 },
-    z: { value: directionalPosition[2], min: -50, max: 50, step: 0.1 },
-    intensity: { value: directionalSetting.intensity, min: 0, max: 10 },
-  });
+  const { x, y, z, intensity } = isDevelopment
+    ? useControls("directionalLight settings", {
+        x: { value: directionalPosition[0], min: -50, max: 50, step: 0.1 },
+        y: { value: directionalPosition[1], min: -50, max: 50, step: 0.1 },
+        z: { value: directionalPosition[2], min: -50, max: 50, step: 0.1 },
+        intensity: { value: directionalSetting.intensity, min: 0, max: 10 },
+      })
+    : {};
 
   useEffect(() => {
-    setDirectionalPosition([x, y, z]);
-    setDirectionalSetting({ intensity });
-  }, [x, y, z, intensity]);
-
-  const { left, right, top, bottom, near, far } = useControls(
-    "orthographicCamera settings",
-    {
-      left: { value: orthoCameraSetting[0], min: -50, max: 50, step: 0.1 },
-      right: { value: orthoCameraSetting[1], min: -50, max: 50, step: 0.1 },
-      top: { value: orthoCameraSetting[2], min: -50, max: 50, step: 0.1 },
-      bottom: { value: orthoCameraSetting[3], min: -50, max: 10 },
-      near: { value: orthoCameraSetting[4], min: -50, max: 50 },
-      far: { value: orthoCameraSetting[5], min: -50, max: 50 },
+    if (isDevelopment) {
+      setDirectionalPosition([x, y, z]);
+      setDirectionalSetting({ intensity });
     }
-  );
+  }, [x, y, z, intensity, isDevelopment]);
+
+  const { left, right, top, bottom, near, far } = isDevelopment
+    ? useControls("orthographicCamera settings", {
+        left: { value: orthoCameraSetting[0], min: -50, max: 50, step: 0.1 },
+        right: { value: orthoCameraSetting[1], min: -50, max: 50, step: 0.1 },
+        top: { value: orthoCameraSetting[2], min: -50, max: 50, step: 0.1 },
+        bottom: { value: orthoCameraSetting[3], min: -50, max: 10 },
+        near: { value: orthoCameraSetting[4], min: -50, max: 50 },
+        far: { value: orthoCameraSetting[5], min: -50, max: 50 },
+      })
+    : {};
 
   useEffect(() => {
-    setOrthoCamera([left, right, top, bottom, near, far]);
-  }, [left, right, top, bottom, near, far]);
+    if (isDevelopment) {
+      setOrthoCamera([left, right, top, bottom, near, far]);
+    }
+  }, [left, right, top, bottom, near, far, isDevelopment]);
 
-  const { xPl, yPl, zPl, powerPl } = useControls("pointLight settings", {
-    xPl: { value: pointCameraSetting[0], min: -50, max: 50, step: 0.1 },
-    yPl: { value: pointCameraSetting[1], min: -50, max: 50, step: 0.1 },
-    zPl: { value: pointCameraSetting[2], min: -50, max: 50, step: 0.1 },
-    powerPl: { value: pointCameraSetting[3], min: 0, max: 1000 },
-  });
+  const { xPl, yPl, zPl, powerPl } = isDevelopment
+    ? useControls("pointLight settings", {
+        xPl: { value: pointCameraSetting[0], min: -50, max: 50, step: 0.1 },
+        yPl: { value: pointCameraSetting[1], min: -50, max: 50, step: 0.1 },
+        zPl: { value: pointCameraSetting[2], min: -50, max: 50, step: 0.1 },
+        powerPl: { value: pointCameraSetting[3], min: 0, max: 1000 },
+      })
+    : {};
 
   useEffect(() => {
-    setPointCamera([xPl, yPl, zPl, powerPl]);
-  }, [xPl, yPl, zPl, powerPl]);
+    if (isDevelopment) {
+      setPointCamera([xPl, yPl, zPl, powerPl]);
+    }
+  }, [xPl, yPl, zPl, powerPl, isDevelopment]);
 
   return (
     <group ref={ref}>
